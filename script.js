8d49586f809d3e87c4facd01595072a9913b9569
Yra795 <yr_ok@mail.ru>
2021-04-07 18:38:49 +0300

    js

--- script.js
+++ script.js
@@ -19,72 +19,34 @@ function fib(num){
 //  alert(fib(15));
 //  alert(fib(68));
 
-function fib(n) {
-    return n <= 1 ? n : fib(n-1)+fib(n-2);
-}
-console.log(fib(15));
-
-
-// function getNumbersModulatorBy(modulus, loggerCallback) {
-
-//     return function(start, end) {
-//         loggerCallback({ message: "Конфигурация", config: configuration });
-//         loggerCallback({ message: "Полученный модулятор", modulus: modulus });
-//         loggerCallback({
-//             message: "Полученный start и end",
-//             start: start,
-//             end: end
-//         });
-
-//         while (start <= end) {
-//             if (start % modulus == configuration.isEntry) {
-//                 loggerCallback(start);
-//             }
-
-//             start++;
-//         }
-//     };
+// function fib(n) {
+//     return n <= 1 ? n : fib(n-1)+fib(n-2);
 // }
-
-// var configuration = {
-//     modulus: 10,
-//     isEntry: false,
-//     start: 45,
-//     end: 68
-// };
-
-// var loggerCallback = data => console.log(data);
-
-// let tenNumbersModulator = getNumbersModulatorBy(
-//     configuration.modulus,
-//     loggerCallback
-// );
-
-// window.configuration.modulus = 5;
-
-// let fiveNumbersModulator = getNumbersModulatorBy(
-//     configuration.modulus,
-//     loggerCallback
-// );
-
-// tenNumbersModulator(configuration.start, configuration.end);
-// tenNumbersModulator(10, 50);
-
-// window.configuration.isEntry = true;
-
-// fiveNumbersModulator(configuration.start, configuration.end);
-// fiveNumbersModulator(10, 21);
+// console.log(fib(15));
 
 
-let userName = prompt ("Пароль?", '');
 
-    if (userName == '1111') {
-    alert('Пароль верный');
-    }
-    else if (pass == '' || pass == null){
-        alert('Отменено');
-    }
-    else {
-        alert('Неверный пароль');
-    }
+// let userName = prompt ("Пароль?", '');
+
+//     if (userName == '1111') {
+//     alert('Пароль верный');
+//     }
+//     else if (pass == '' || pass == null){
+//         alert('Отменено');
+//     }
+//     else {
+//         alert('Неверный пароль');
+//     }
+
+
+
+var buttons = document.querySelectorAll('button');
+
+for (var i = 0; i < buttons.length; i++){
+    buttons[i].onclick = function(x){
+        return function(){
+            console.log(x);
+        }
+    }(i);
+}
 
