a51b7c2d84391e5dcd848a424edb725f3f1ad7d2
Yra795 <yr_ok@mail.ru>
2021-04-17 09:03:22 +0300

    Burger

---
+++ uno/script.js
@@ -0,0 +1,100 @@
+"use strict"
+
+console.log(45);
+
+function fib(num){
+    let a = 1,
+        b = 1;
+
+    for (let i = 3; i <= num; i++) {
+        let c = a + b;
+            a = b;
+            b = c;
+    }
+    return b;
+}
+ console.log(fib(10));
+
+//  alert(fib(8));
+//  alert(fib(15));
+//  alert(fib(68));
+
+// function fib(n) {
+//     return n <= 1 ? n : fib(n-1)+fib(n-2);
+// }
+// console.log(fib(15));
+
+
+
+// let userName = prompt ("Пароль?", '');
+
+//     if (userName == '1111') {
+//     alert('Пароль верный');
+//     }
+//     else if (pass == '' || pass == null){
+//         alert('Отменено');
+//     }
+//     else {
+//         alert('Неверный пароль');
+//     }
+
+
+
+var buttons = document.querySelectorAll('button');
+
+for (var i = 0; i < buttons.length; i++){
+    buttons[i].onclick = function(x){
+        return function(){
+            console.log(x);
+        }
+    }(i);
+}
+
+
+let passport = [
+{
+    name:"Ivan",
+    lastName:"Ivanov",
+    fatherName:"Ivanovich",
+    birdhdate:"01.01.1970",
+    registrationCountry:"Belarus",
+    registracionCity:"Minsk",
+    registracionAddress:"Lenina st.,21",
+    issuedByDepartment:"Leninsky district police department",
+    dateOfIssue:"",
+    validBefore:"",
+},
+
+{
+    name:"Petia",
+    lastName:"Petrov",
+    fatherName:"Petrovich",
+    birdhdate:"06.09.1990",
+    registrationCountry:"Belarus",
+    registracionCity:"Brest",
+    registracionAddress:"Lenina st.,21",
+    issuedByDepartment:"Leninsky district police department",
+    dateOfIssue:"",
+    validBefore:"",
+}
+];
+
+
+function sortByParam(passportList, paramName){
+    let sortFunc = function(a, b){
+        if (a[paramName] > b[paramName]) {return 1;}
+        if (a[paramName] < b[paramName]) {return -1;}
+        return 0;}
+    return passportList.sort(sortFunc);
+}
+sortByParam(passport, "name"); 
+
+
+//  passport.sort(function(a, b){
+//     if (a.name > b.name) {return 1;}
+//     if (a.name < b.name) {return -1;}
+//     return 0;
+// })
+console.log(passport); 
+
+
